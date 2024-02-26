9abbb43c81e2e21993220c77872b871ef92f30f5
bjr6544 <111366770+bjr6544@users.noreply.github.com>
2024-02-25 19:06:01 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ apt upgrade -y
 apt install net-tools
 apt install unattended-upgrades
 systemctl enable --now unattended-upgrades
+wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/kickstart.sh && sh /tmp/netdata-kickstart.sh
+sudo systemctl start netdata
