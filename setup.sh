0dabbb39101d6f71580d00e204158a03e1500211
bjr6544 <111366770+bjr6544@users.noreply.github.com>
2024-02-28 19:49:16 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ apt install unattended-upgrades
 systemctl enable --now unattended-upgrades
 wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/kickstart.sh && sh /tmp/netdata-kickstart.sh
 sudo systemctl start netdata
+apt get-install iperf3
