e355ed28ab02c9c671841c29f9a9dc0be732dfe4
bjr6544 <111366770+bjr6544@users.noreply.github.com>
2024-03-09 09:30:55 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 apt update
 apt upgrade -y
-apt install net-tools
+apt install net-tools -y
 apt install unattended-upgrades
 systemctl enable --now unattended-upgrades
 wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/kickstart.sh && sh /tmp/netdata-kickstart.sh
-sudo systemctl start netdata
-apt get-install iperf3
-sudo apt install neofetch
+sudo systemctl start netdata 
+apt get-install iperf3 -y
+sudo apt install neofetch -y
 neofetch
